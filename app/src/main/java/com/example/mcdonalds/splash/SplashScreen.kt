fc91b681f666aa4ff960ba3496f31c8204c11797
Alex Testa <alex.testa@studio.unibo.it>
2022-04-18 15:48:18 +0200

    Bug Fixed

--- app/src/main/java/com/example/mcdonalds/splash/SplashScreen.kt
+++ app/src/main/java/com/example/mcdonalds/splash/SplashScreen.kt
@@ -11,6 +11,7 @@ import com.example.mcdonalds.MainActivity
 import com.example.mcdonalds.R
 import com.example.mcdonalds.utils.Constants
 import com.example.mcdonalds.utils.Permission
+import kotlin.system.exitProcess
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,22 +21,22 @@ class SplashScreen : AppCompatActivity() {
         //Hide ActionBar
         supportActionBar?.hide()
 
+        //Check Internet Status
         if(Permission.checkNetworkIsEnabled(this@SplashScreen)){
             //Start MainActivity
             Handler(Looper.getMainLooper()).postDelayed({
-                val openMainActivity = Intent(this@SplashScreen, MainActivity::class.java)
-                startActivity(openMainActivity)}
+                startActivity(Intent(this@SplashScreen, MainActivity::class.java))}
                 , Constants.SPLASH_SCREEN_DURATION)
         }else{
             //If the phone is offline
             //Create Dialog for open the internet settings
             //Else close app
             AlertDialog.Builder(this@SplashScreen)
-                .setTitle("La connessione dati non è attiva")
-                .setMessage("Vuoi attivarla?")
+                .setTitle(getString(R.string.connection_error))
+                .setMessage(getString(R.string.connection_error_message))
                 .setIcon(R.drawable.ic_not_signals)
-                .setPositiveButton("Si"){_,_ -> startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))}
-                .setNegativeButton("No") {_,_ -> System.exit(1)}
+                .setPositiveButton(getString(R.string.confirm)){ _, _ -> startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))}
+                .setNegativeButton(getString(R.string.not_confirm)) { _, _ -> exitProcess(1) }
                 .setCancelable(false)
                 .show()
         }
