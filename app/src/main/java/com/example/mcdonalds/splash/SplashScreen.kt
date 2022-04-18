a1442d9abf329373270c8efccc6b1104762c3e22
Alex Testa <alex.testa@studio.unibo.it>
2022-04-18 15:38:49 +0200

    Add new icon, control newtwork state

--- app/src/main/java/com/example/mcdonalds/splash/SplashScreen.kt
+++ app/src/main/java/com/example/mcdonalds/splash/SplashScreen.kt
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import com.example.mcdonalds.MainActivity
 import com.example.mcdonalds.R
 import com.example.mcdonalds.utils.Constants
+import com.example.mcdonalds.utils.Permission
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +19,25 @@ class SplashScreen : AppCompatActivity() {
 
         //Hide ActionBar
         supportActionBar?.hide()
-        //Start MainActivity
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val openMainActivity = Intent(this@SplashScreen, MainActivity::class.java)
-            startActivity(openMainActivity)}
-            , Constants.SPLASH_SCREEN_DURATION)
+        if(Permission.checkNetworkIsEnabled(this@SplashScreen)){
+            //Start MainActivity
+            Handler(Looper.getMainLooper()).postDelayed({
+                val openMainActivity = Intent(this@SplashScreen, MainActivity::class.java)
+                startActivity(openMainActivity)}
+                , Constants.SPLASH_SCREEN_DURATION)
+        }else{
+            //If the phone is offline
+            //Create Dialog for open the internet settings
+            //Else close app
+            AlertDialog.Builder(this@SplashScreen)
+                .setTitle("La connessione dati non è attiva")
+                .setMessage("Vuoi attivarla?")
+                .setIcon(R.drawable.ic_not_signals)
+                .setPositiveButton("Si"){_,_ -> startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))}
+                .setNegativeButton("No") {_,_ -> System.exit(1)}
+                .setCancelable(false)
+                .show()
+        }
     }
 }
