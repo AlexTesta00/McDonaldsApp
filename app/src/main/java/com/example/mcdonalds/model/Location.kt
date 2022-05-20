18e0701ff0470975a80c22b95e9c587b5be113fa
Alex Testa <alex.testa@studio.unibo.it>
2022-05-20 02:40:59 +0200

    Adjust Recycler View, Add Complete Order Screen

--- app/src/main/java/com/example/mcdonalds/model/Location.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mcdonalds.model
-
-interface Location {
-    fun getLatitude() : Double
-    fun getLongitude() : Double
-    fun getMcName() : String
-    fun isOpen() : Boolean
-}
---
+++ app/src/main/java/com/example/mcdonalds/model/QRGenerator.kt
@@ -0,0 +1,4 @@
+package com.example.mcdonalds.model
+
+class QRGenerator {
+}
